a09d9b30ac35755cfede362efb24a77346a957f8
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-02-23 15:07:52 +0100

    add new files

--- js/ajax2.js
+++ js/ajax2.js
@@ -1,19 +1,32 @@
-let url = 'http://api.icndb.com/jokes/random';
+$(function() {
 
-let button = document.getElementById('get-joke');
-let paragraph = document.getElementById('joke');
-button.addEventListener('click', function() {
-	getJoke();
-})
+let tweetLink = 'https://twitter.com/intent/tweet?text=', quoteUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?';
 
-function getJoke() {
-	let xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function() {
-		let response = JSON.parse(xhr.response);
-		paragraph.innerText = response.value.joke;
-	});
-	xhr.send();
+function getQuote() {
+	$.getJSON(quoteUrl, createTweet);
 }
-// dla chętnych :-)
-getJoke();
+
+function createTweet(input) {
+	if(!input.quoteAuthor.length) {
+		input.quoteAuthor = 'Unknown author';
+	}
+
+	let tweetText = 'Quote of the day - ' + input.quoteText + 'Author: ' + input.quoteAuthor;
+
+	if (tweetText.length > 140) {
+		getQuote();
+	} else {
+		let tweet = tweetLink + encodeURIComponent(tweetText);
+		$('.quote p').text(input.quoteText);
+		$('.author h2').text("Author: " + input.quoteAuthor);
+		$('.tweet').attr('href', tweet);
+	}
+}
+
+$('.trigger').click(function() {
+	getQuote();
+});
+
+getQuote();
+
+});
