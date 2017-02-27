ddfcd497d05f46bab45c5b4dd4f01c262c65ed2f
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-02-27 19:15:12 +0100

    fixed bug

--- js/ajax2.js
+++ js/ajax2.js
@@ -1,32 +1,29 @@
 $(function() {
 
-let tweetLink = 'https://twitter.com/intent/tweet?text=', quoteUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?';
+	const tweetLink = 'https://twitter.com/intent/tweet?text=', quoteUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?';
 
-function getQuote() {
-	$.getJSON(quoteUrl, createTweet);
-}
-
-function createTweet(input) {
-	if(!input.quoteAuthor.length) {
-		input.quoteAuthor = 'Unknown author';
+	function getQuote() {
+		$.getJSON(quoteUrl, createTweet);
 	}
 
-	let tweetText = 'Quote of the day - ' + input.quoteText + 'Author: ' + input.quoteAuthor;
+	function createTweet(input) {
+		if(!input.quoteAuthor.length) {
+			input.quoteAuthor = 'Unknown author';
+		}
 
-	if (tweetText.length > 140) {
-		getQuote();
-	} else {
-		let tweet = tweetLink + encodeURIComponent(tweetText);
-		$('.quote p').text(input.quoteText);
-		$('.author h2').text("Author: " + input.quoteAuthor);
-		$('.tweet').attr('href', tweet);
+		let tweetText = 'Quote of the day - ' + input.quoteText + 'Author: ' + input.quoteAuthor;
+
+		if (tweetText.length > 140) {
+			getQuote();
+		} else {
+			let tweet = tweetLink + encodeURIComponent(tweetText);
+			$('.quote p').text(input.quoteText);
+			$('.author h2').text("Author: " + input.quoteAuthor);
+			$('.tweet').attr('href', tweet);
+		}
 	}
-}
 
-$('.trigger').click(function() {
+	$('.trigger').click(getQuote);
 	getQuote();
-});
-
-getQuote();
 
 });
